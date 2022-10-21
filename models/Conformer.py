0225aa5edb7a578b688c0299ee931016b5d6eebe
kawa <180441035@ccalumni.meijo-u.ac.jp>
2022-10-21 17:08:09 +0900

    Conformerのモジュールらをクラスに移行

--- models/Conformer.py
+++ models/Conformer.py
@@ -11,12 +11,11 @@ from tensorflow.keras.layers import (
     MultiHeadAttention,
     Dense,
     Dropout,
-    Flatten,
+    Layer,
     Input,
     MaxPool2D,
     Permute,
     Reshape,
-    SeparableConv2D,
 )
 import tensorflow as tf
 
@@ -26,50 +25,118 @@ class Conformer(DNN):
     class Params(ModelParams):
         encoder_layers: int = field(default=16)
         encoder_dim: int = field(default=256)
+        ffn_inner_dim_factor: int = field(default=4)
         attention_heads: int = field(default=4)
         kernel_size: int = field(default=32)
         dropout_rate: float = field(default=0.1)
 
-    def definition(self, param: ModelParams) -> Model:
+    def definition(self, param: Params) -> Model:
         return super().definition(param)
 
-    def conformer_block(self, x: tf.Tensor, layer: int) -> tf.Tensor:
-        y1 = self.feed_forward_module(x, 1, layer)(x)
-        y1 = tf.math.scalar_mul(0.5, y1, name="ffn1-half_%d" % layer)
-        y1 = Add(name="ffn1-Add_%d" % layer)((x, y1))
-        y2 = self.multi_head_self_attention_module(x, layer)(y1)
-        y2 = Add(name="mha-Add_%d" % layer)((y1, y2))
-        y3 = self.convolution_module(x, layer)(y2)
-        y3 = Add(name="conv-Add_%d" % layer)((y2, y3))
-        y4 = self.feed_forward_module(x, 1, layer)(y3)
-        y4 = tf.math.scalar_mul(0.5, y4, name="ffn1-half_%d" % layer)
-        y4 = Add(name="ffn2-Add_%d" % layer)((y3, y4))
-        y4 = LayerNormalization(name="conf-LN_%d" % layer)(y4)
 
-        return y4
+class FeedForwardModule(Layer):
+    def __init__(
+        self,
+        layer_num: int,
+        input_dim: int,
+        inner_dim_factor: int = 4,
+        dropout_rate: float = 0.1,
+        scale_factor: float = 0.5,
+        trainable=True,
+        name="ffn",
+        dtype=None,
+        dynamic=False,
+        **kwargs
+    ) -> None:
+        super().__init__(trainable, name, dtype, dynamic, **kwargs)
 
-    def feed_forward_module(self, x: tf.Tensor, num: int, layer: int) -> tf.Tensor:
-        y = LayerNormalization(name="ffn-LN_%d" % layer)(x)
-        y = Dense(name="ffn%d-D1_%d" % (num, layer), activation="swish")(y)
-        y = Dropout(name="ffn%d-DO1_%d" % (num, layer))(y)
-        y = Dense(name="ffn%d-D2_%d" % (num, layer))(y)
-        y = Dropout(name="ffn%d-DO2_%d" % (num, layer))(y)
+        self.inner_dim_factor = inner_dim_factor
+        self.scale_factor = scale_factor
 
-        return y
+        self.ln = LayerNormalization(name="%s-LN-%d" % (name, layer_num))
+        self.ffn1 = Dense(
+            input_dim * inner_dim_factor,
+            name="%s-in-D1-%d" % (name, layer_num),
+            activation="swish",
+        )
+        self.do1 = Dropout(dropout_rate, name="%s-DO1-%d" % (name, layer_num))
+        self.ffn2 = Dense(name="%s-out-D2-%d" % (name, layer_num))
+        self.do2 = Dropout(dropout_rate, name="%s-DO2-%d" % (name, layer_num))
+        self.res_add = Add(name="%s-Add-%d" % (name, layer_num))
 
-    def multi_head_self_attention_module(self, x: tf.Tensor, layer: int) -> tf.Tensor:
-        y = LayerNormalization(name="mha-LN_%d" % layer)(x)
-        y = MultiHeadAttention(name="mha-MHA_%d" % layer)(y)
-        y = Dropout(name="mha-DO_%d" % layer)(y)
+    def call(self, inputs, training=False) -> tf.Tensor:
+        outputs = self.ln(inputs, training=training)
+        outputs = self.ffn1(outputs, training=training)
+        outputs = self.do1(outputs, training=training)
+        outputs = self.ffn2(outputs, training=training)
+        outputs = self.do2(outputs, training=training)
+        outputs = self.res_add([inputs, self.scale_factor * outputs])
 
-        return y
+        return outputs
 
-    def convolution_module(self, x: tf.Tensor, layer: int) -> tf.Tensor:
-        y = LayerNormalization(name="conv-LN_%d" % layer)(x)
-        y = Conv2D(name="conv-DwC_%d" % layer)(y)
-        y = GLU(name="conv-GLU")(y)
-        y = BatchNormalization(name="conv-BN_%d" % layer)(y)
-        y = Conv2D(name="conv-PwC_%d" % layer)(y)
-        y = Dropout(name="conv-DO_%d" % layer)(y)
 
-        return y
+class ConvolutionModule(Layer):
+    def __init__(
+        self,
+        layer_num: int,
+        trainable=True,
+        name="conv",
+        dtype=None,
+        dynamic=False,
+        **kwargs
+    ) -> None:
+        super().__init__(trainable, name, dtype, dynamic, **kwargs)
+
+        self.ln = LayerNormalization(name="%s-LN-%d" % (name, layer_num))
+        self.pw_conv = Conv2D(name="%s-DwC-%d" % (name, layer_num))
+        self.glu = GLU(name="%s-GLU-%d" % (name, layer_num))
+        self.bn = BatchNormalization(name="%s-BN-%d" % (name, layer_num))
+        self.dw_conv = Conv2D(name="%s-PwC-%d" % (name, layer_num))
+        self.do = Dropout(name="%s-DO-%d" % (name, layer_num))
+        self.res_add = Add(name="%s-Add-%d" % (name, layer_num))
+
+    def call(self, inputs, *args, **kwargs) -> tf.Tensor:
+        return super().call(inputs, *args, **kwargs)
+
+
+class MultiHeadAttentionModule(Layer):
+    def __init__(
+        self,
+        layer_num: int,
+        trainable=True,
+        name="mha",
+        dtype=None,
+        dynamic=False,
+        **kwargs
+    ) -> None:
+        super().__init__(trainable, name, dtype, dynamic, **kwargs)
+
+        self.ln = LayerNormalization(name="%s-LN-%d" % (name, layer_num))
+        self.rel_mha = MultiHeadAttention(name="%s-MHA-%d" % (name, layer_num))
+        self.do = Dropout(name="%s-DO-%d" % (name, layer_num))
+        self.res_add = Add(name="%s-Add-%d" % (name, layer_num))
+
+    def call(self, inputs, *args, **kwargs) -> tf.Tensor:
+        return super().call(inputs, *args, **kwargs)
+
+
+class ConformerModule(Layer):
+    def __init__(
+        self,
+        layer_num: int,
+        trainable=True,
+        name="conf",
+        dtype=None,
+        dynamic=False,
+        **kwargs
+    ):
+        super().__init__(trainable, name, dtype, dynamic, **kwargs)
+
+        self.ffn1 = FeedForwardModule()
+        self.mha = MultiHeadAttentionModule()
+        self.conv = ConvolutionModule()
+        self.ffn2 = FeedForwardModule()
+        self.ln = LayerNormalization(name="%s-LN-%d" % (name, layer_num))
+
+    def call(self, inputs, *args, **kwargs) -> tf.Tensor:
+        return super().call(inputs, *args, **kwargs)
